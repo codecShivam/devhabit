c7cc6a60078ff8f0381465f333d719275c8d339a
codecShivam <codecshivam@gmail.com>
2023-09-05 11:32:07 +0530

    integrated material tailwind for fast development

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavbarMenu";
 import RoadmapGenerator from "../components/RoadmapGenerator";
 import { RoadmapProvider } from "../context/RoadmapContext";
 
