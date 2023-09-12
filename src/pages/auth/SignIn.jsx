5ecfa1768ad8c0315eb80fa389de9a1228731ff7
codecShivam <codecshivam@gmail.com>
2023-09-12 14:46:44 +0530

    fixed error

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -29,21 +29,28 @@ export function SignIn() {
   };
 
   return (
-    <div className="bg-blue-500 h-screen flex flex-col justify-center items-center">
-      <h1 className="text-4xl text-white font-semibold mb-8">Welcome to DevHabit</h1>
+    <>
+      {" "}
       {user ? (
         <Home />
       ) : (
-        <div>
-          <button
-            className="bg-white text-blue-500 hover:bg-blue-600 text-lg font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 hover:text-white"
-            onClick={handleSignIn}
-          >
-            Login with Google
-          </button>
-          <p className="mt-4 text-white text-lg">To proceed, please sign in with your Google account.</p>
+        <div className="bg-blue-500 h-screen flex flex-col justify-center items-center">
+          <h1 className="text-4xl text-white font-semibold mb-8">
+            Welcome to DevHabit
+          </h1>
+          <div>
+            <button
+              className="bg-white text-blue-500 hover:bg-blue-600 text-lg font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 hover:text-white"
+              onClick={handleSignIn}
+            >
+              Login with Google
+            </button>
+            <p className="mt-4 text-white text-lg">
+              To proceed, please sign in with your Google account.
+            </p>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
